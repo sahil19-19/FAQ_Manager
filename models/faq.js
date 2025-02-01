6669fb497894c57fd7d87df9eff66e44eae09ef8
Sahil <sahilverma1931@gmail.com>
2025-02-01 08:09:12 +0530

    add model faq

---
+++ models/faq.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const faqSchema = new mongoose.Schema({
+    question: {
+        type: String, 
+        required: true 
+    },
+    answer: { 
+        type: String, 
+        required: true
+    }, // for WYSIWYG support
+    question_bn: {
+        type: String, 
+        required: true 
+    },
+    answer_bn: { 
+        type: String, 
+        required: true
+    },
+    question_hi: {
+        type: String, 
+        required: true 
+    },
+    answer_bn : { 
+        type: String, 
+        required: true
+    },
+});
+
+// to get translated text dynamically
+faqSchema.methods.getTranslation = function (lang = "en") {
+    // return this.translations[lang] || this.translations["en"];
+    return this.translations[lang]
+};
+
+module.exports = mongoose.model("faq", faqSchema)
