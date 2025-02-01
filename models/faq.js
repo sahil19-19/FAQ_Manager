d180c6b85ad0a7046b884d41c6844e3aeef1de61
Sahil <sahilverma1931@gmail.com>
2025-02-01 08:11:23 +0530

    fix: correct typo in faq.js

--- models/faq.js
+++ models/faq.js
@@ -2,35 +2,35 @@ const mongoose = require("mongoose");
 
 const faqSchema = new mongoose.Schema({
     question: {
-        type: String, 
-        required: true 
+        type: String,
+        required: true,
     },
-    answer: { 
-        type: String, 
-        required: true
+    answer: {
+        type: String,
+        required: true,
     }, // for WYSIWYG support
     question_bn: {
-        type: String, 
-        required: true 
+        type: String,
+        required: true,
     },
-    answer_bn: { 
-        type: String, 
-        required: true
+    answer_bn: {
+        type: String,
+        required: true,
     },
     question_hi: {
-        type: String, 
-        required: true 
+        type: String,
+        required: true,
     },
-    answer_bn : { 
-        type: String, 
-        required: true
+    answer_hi: {
+        type: String,
+        required: true,
     },
 });
 
 // to get translated text dynamically
 faqSchema.methods.getTranslation = function (lang = "en") {
     // return this.translations[lang] || this.translations["en"];
-    return this.translations[lang]
+    return this.translations[lang];
 };
 
-module.exports = mongoose.model("faq", faqSchema)
+module.exports = mongoose.model("faq", faqSchema);
