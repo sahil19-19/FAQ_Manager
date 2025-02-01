c33e412d69f2e044d822969e9cb8149a7b5f373d
Sahil <sahilverma1931@gmail.com>
2025-02-01 09:44:06 +0530

    fix : schema of model to store translations in a map

--- models/faq.js
+++ models/faq.js
@@ -9,27 +9,27 @@ const faqSchema = new mongoose.Schema({
         type: String,
         required: true,
     }, // for WYSIWYG support
-    question_bn: {
-        type: String,
-        required: true,
-    },
-    answer_bn: {
-        type: String,
-        required: true,
-    },
-    question_hi: {
-        type: String,
-        required: true,
-    },
-    answer_hi: {
-        type: String,
-        required: true,
-    },
+    translations : { // a map
+        bn : {
+            question_bn : String,
+            answer_bn : String
+        },
+        hi : {
+            question_hi : String,
+            answer_hi : String
+        }
+    }
 });
 
 // to get translated text dynamically
 faqSchema.methods.getTranslation = function (lang = "en") {
     // return this.translations[lang] || this.translations["en"];
+    if(lang === "en"){ 
+        return {
+            question : this.question,
+            answer : this.answer
+        }
+    }
     return this.translations[lang];
 };
 
