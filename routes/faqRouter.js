87b4f898e17e2de6f6345ce037cd23734afbf96b
Sahil <sahilverma1931@gmail.com>
2025-02-01 18:01:34 +0530

    fix : Add middlewate getId in delete call

--- routes/faqRouter.js
+++ routes/faqRouter.js
@@ -3,11 +3,13 @@ const router = express.Router();
 // const {get_faqs, create_faq, delete_faq} = require('../controllers')
 const create_faq = require('../controllers/createFaq');
 const delete_faq = require('../controllers/deleteFaq');
+const get_faq = require('../controllers/getFaq');
+const get_id = require('../middleware/getId')
 
-// router.get('/',get_faqs);
+router.get('/',get_faq);
 
 router.post('/',create_faq);
 
-router.delete('/:id',delete_faq);
+router.delete('/:id', get_id, delete_faq);
 
 module.exports = router;
