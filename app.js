fabde0c82ae19687ac98c7e8278eaa4028266db9
Sahil <sahilverma1931@gmail.com>
2025-02-01 07:59:23 +0530

    feat: Add router to handle api calls

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
-require('dotenv').config();
 const db = require('./config/mongoDb')
+const faq_route = require('./routes/faqRouter')
+require('dotenv').config();
 
 const app = express()
 app.use(express.json());
-
 db();
+
+app.use('/api/faqs',faq_route);
 
 const PORT = process.env.PORT | 3000 ;
 
---
+++ routes/faqRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',get_faqs);
+
+
+module.exports = router;
