8de87501bd7360c5f9ff509c1e87dbded7d693d5
Sahil <sahilverma1931@gmail.com>
2025-02-02 15:43:45 +0530

    fix : Remove unnecesary lines from redis.js

--- config/redis.js
+++ config/redis.js
@@ -6,13 +6,6 @@ const redisClient = Redis.createClient({
     port: process.env.REDIS_PORT || 6379,
 });
 
-// redisClient.on("error", (error) => {
-//     console.error("Redis connection error:", error);
-// });
-
-// redisClient.on("connect", () => {
-//     console.log("Redis connected successfully");
-// });
 redisClient.connect();
 
 module.exports = redisClient;
